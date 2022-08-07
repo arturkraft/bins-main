0a473a59ac4384fc105173b055c15c2956e337b4
Artur Kraft <artur.kraft@outlook.com>
2022-08-07 11:12:17 +0100

    branch issues

--- html-head.php
+++ html-head.php
@@ -4,9 +4,13 @@
 <head>
     <title>Bins collection day - <?php echo $location_name; ?></title>
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 
+=======
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+>>>>>>> c221532 (experiment 2 finished)
 =======
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 >>>>>>> c221532 (experiment 2 finished)
@@ -61,6 +65,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
     
@@ -73,6 +78,8 @@
   } );
   </script>
 =======
+=======
+>>>>>>> c221532 (experiment 2 finished)
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -81,6 +88,9 @@
       $( "#tabs" ).tabs();
     } );
     </script>
+<<<<<<< HEAD
+>>>>>>> c221532 (experiment 2 finished)
+=======
 >>>>>>> c221532 (experiment 2 finished)
     
 <?php
@@ -88,6 +98,7 @@
 echo $gtag;
 
 ?>
+<<<<<<< HEAD
 <<<<<<< HEAD
 <style>
         :root {
@@ -181,6 +192,8 @@ echo $gtag;
     }
 </style>
 =======
+=======
+>>>>>>> c221532 (experiment 2 finished)
 
     <style>
             :root {
@@ -267,5 +280,8 @@ echo $gtag;
             ?>;
         }
     </style>
+<<<<<<< HEAD
+>>>>>>> c221532 (experiment 2 finished)
+=======
 >>>>>>> c221532 (experiment 2 finished)
 </head>
