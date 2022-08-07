abf6209133ceaa59ccc31f881c72ef1e004436a5
Artur Kraft <artur.kraft@outlook.com>
2022-08-07 11:16:47 +0100

    final branch fix

--- html-head.php
+++ html-head.php
@@ -3,17 +3,7 @@
 
 <head>
     <title>Bins collection day - <?php echo $location_name; ?></title>
-<<<<<<< HEAD
-<<<<<<< HEAD
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
-
-=======
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
->>>>>>> c221532 (experiment 2 finished)
-=======
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
->>>>>>> c221532 (experiment 2 finished)
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="theme-color" content="#fff" media="(prefers-color-scheme: light)">
     <meta name="theme-color" content="#000" media="(prefers-color-scheme: dark)">
@@ -64,22 +54,6 @@
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.2/main.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-    
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script>
-  $( function() {
-    $( "#tabs" ).tabs();
-  } );
-  </script>
-=======
-=======
->>>>>>> c221532 (experiment 2 finished)
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
@@ -88,112 +62,12 @@
       $( "#tabs" ).tabs();
     } );
     </script>
-<<<<<<< HEAD
->>>>>>> c221532 (experiment 2 finished)
-=======
->>>>>>> c221532 (experiment 2 finished)
     
 <?php
 
 echo $gtag;
 
 ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<style>
-        :root {
-        --background-primary: #fff;
-        --color-primary: #333;
-         transition: color 300ms, background-color 300ms;
-    }
-
-    html[data-theme='dark'] {
-        --background-primary: #6f6f6f;
-        --color-primary: #333;
-         transition: color 300ms, background-color 300ms;
-    }
-
-
-    html {
-                background: var(--background-primary);
-        color: var(--color-primary);
-        transition: color 300ms, background-color 300ms;
-
-    }
-
-    .ios-peek {
-    position: fixed;
-    width: 505px;
-    height: 500px;
-    z-index: -1;
-    top: 0;
-    left: 0;
-    background-color: red !important;
-    }
-
-    * {
-      margin: 0;
-    }
-
-    #content {
-        min-height: 100vh;
-        height: 100%;
-        background-color: #fff;
-    }
-
-    #bottom-text {
-        position: absolute;
-        bottom: 0;
-    }
-
-    h1,
-    h2,
-    h3,
-    h4,
-    p {
-        color: var(--color-primary);
-
-    }
-
-    html {
-        transition: color 300ms, background-color 300ms !important;
-    }
-
-    html[data-theme='dark'] {
-        background: #000;
-        transition: color 300ms, background-color 300ms !important;
-        filter: invert(1) hue-rotate(180deg);
-        height: 100%;
-        margin: 0;
-        padding: 0;
-    }
-
-    html[data-theme='dark'] img {
-        filter: invert(1) hue-rotate(180deg)
-    }
-    .grey {
-        color: <?php echo $grey->getColour();
-        ?>;
-    }
-
-    .blue {
-        color: <?php echo $blue->getColour();
-        ?>;
-    }
-
-    .green {
-        color: <?php echo $green->getColour();
-        ?>;
-    }
-
-    .brown {
-        color: <?php echo $brown->getColour();
-        ?>;
-    }
-</style>
-=======
-=======
->>>>>>> c221532 (experiment 2 finished)
 
     <style>
             :root {
@@ -280,8 +154,4 @@ echo $gtag;
             ?>;
         }
     </style>
-<<<<<<< HEAD
->>>>>>> c221532 (experiment 2 finished)
-=======
->>>>>>> c221532 (experiment 2 finished)
 </head>
